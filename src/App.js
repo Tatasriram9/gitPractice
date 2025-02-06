3e37f49a5d10dd4be3edb1af16fef2aba6a71efa
Harikrishna <harikrishnakaratla12@gmail.com>
2025-02-06 14:19:24 +0530

    add faq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Faq from './components/Faq';
 
 function App() {
   return (
     <>
+    
       <Navbar />
+      <Faq/>
     </>
   );
 }
