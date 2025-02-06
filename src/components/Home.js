cc7dd23a53de9e0227d994dae70ce0ef015ba4bd
ThirupathiAbbaravena <abbaravenathirupathi17@gmail.com>
2025-02-06 14:25:06 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import image from  "../assits/images/background.png";
+
+
+function App(){
+    return(
+        <>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-4">
+                    <img src={image}></img>
+                </div>
+               
+                
+
+
+            </div>
+
+        </div>
+        </>
+    )
+}
+export default App;
